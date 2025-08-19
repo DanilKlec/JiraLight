2a8e8fe563a4a3f82b0fadd6122c258e9f0cf3fe
danil <dev_stager@KRYUKOVA>
2025-08-19 16:23:14 +0300

    Fix

--- JiraLight/JiraLight.Desktop/Views/MainWindow.axaml.cs
+++ JiraLight/JiraLight.Desktop/Views/MainWindow.axaml.cs
@@ -1,3 +1,5 @@
+using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.ReactiveUI;
 using JiraLight.Desktop.ViewModels;
 using ReactiveUI;
@@ -10,5 +12,22 @@ public partial class MainWindow : ReactiveWindow<MainViewModel>
     {
         InitializeComponent();
         this.WhenActivated(d => { /* Подписки здесь */ });
+
+        // Назначаем обработчики событий в конструкторе
+        this.FindControl<Button>("MinimizeButton")?.AddHandler(Button.ClickEvent, (_, __) =>
+            this.WindowState = Avalonia.Controls.WindowState.Minimized);
+
+        this.FindControl<Button>("MaximizeButton")?.AddHandler(Button.ClickEvent, (_, __) =>
+            this.WindowState = this.WindowState == Avalonia.Controls.WindowState.Maximized
+                ? Avalonia.Controls.WindowState.Normal
+                : Avalonia.Controls.WindowState.Maximized);
+
+        this.FindControl<Button>("CloseButton")?.AddHandler(Button.ClickEvent, (_, __) =>
+            this.Close());
+    }
+
+    private void TopBar_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        BeginMoveDrag(e);
     }
 }
